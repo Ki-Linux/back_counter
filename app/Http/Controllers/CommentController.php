18e88a936c75feabfc3305ed0315a5566e480004
Ki-Linux <linuxseima@gmail.com>
2022-04-14 21:02:03 +0900

    comment

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -33,22 +33,24 @@ class CommentController extends Controller
 
         for($i=0; $i < count($get_pointed_comment); $i++) {//名前が重複しないように配列に入れる
 
-            if(in_array($get_pointed_comment[$i]->username, $only_name_data)) {
+            $user_name = $get_pointed_comment[$i]->username;
+
+            if(in_array($user_name, $only_name_data)) {
 
                 continue;
 
             }
-            array_push($only_name_data, $get_pointed_comment[$i]->username);
+            array_push($only_name_data, $user_name);
         }
 
         $name_icon_array = array();
 
-        for($j=0; $j < count($only_name_data); $j++) {
-            array_push($name_icon_array, ["username" => $get_pointed_comment[$j]->username, "icon" => "img_data"]);
+        for($j=0; $j < count($only_name_data); $j++) {//名前と画像の連想配列を作る
+            array_push($name_icon_array, ["username" => $get_pointed_comment[$j]->username, "icon" => $j]);
         }
 
 
 
-        return ['ui' => $name_icon_array];
+        return ['name_icon' => $name_icon_array, 'name_comment' => $get_pointed_comment];
     }
 }
