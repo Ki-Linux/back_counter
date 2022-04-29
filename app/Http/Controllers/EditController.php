e1f6c83b078052ec25d50100cd7d946e75e1ceca
Ki-Linux <linuxseima@gmail.com>
2022-04-29 15:03:17 +0900

    edit eDITCONTROLLER

--- app/Http/Controllers/EditController.php
+++ app/Http/Controllers/EditController.php
@@ -90,20 +90,27 @@ class EditController extends Controller
     {
         $edit = new Edit();
 
-        //$which_data = $response->id;
-        //$sql_data = 'id';
-        //$array_send_data = ['*'];
+        //if($response->target == 'edit') {
+            $sql_data;
+            $reference_data;
+            $array_send_data;
+    
 
-        
-
-        //if($response->username) {
+        if($response->target == 'list') {
            // $user_content = Edit::where('username', $response->username)->get(['id', 'picture', 'my_comment', 'updated_at']);
-           $username = $response->username;
-           //$sql_data = 'username';
+           $sql_data = 'username';
+           $reference_data = $response->username;
            $array_send_data = ['id', 'picture', 'my_comment', 'can_see', 'created_at'];
-        //}
 
-        $user_content = Edit::where('username', $username)->get($array_send_data);
+        } else if($response->target == 'edit') {
+
+            $sql_data = 'id';
+            $reference_data = $response->id;
+            $array_send_data = ['id', 'username', 'picture', 'my_comment', 'can_top', 'can_see', 'can_list', 'can_good', 'can_comment'];
+
+        }
+
+        $user_content = Edit::where($sql_data, $reference_data)->get($array_send_data);
 
 
         return ['contents' => $user_content];
@@ -121,18 +128,31 @@ class EditController extends Controller
     public function update(Request $request, $id)
     {
 
-        $user_content = Edit::where('id', $id)
+        /*$user_content = Edit::where('id', intval($id))
                             ->update([
+                                'username' => $request->username,
                                 'picture' => $request->image,
                                 'my_comment' => $request->comment,
-                                'can_list' => $request->can_list,
                                 'can_good' => $request->show_good,
                                 'can_comment' => $request->others_comment,
                                 'can_see' => $request->can_see,
                                 'can_top' => $request->to_top,
-                            ]);
+                                'can_list' => $request->can_list, 
+                            ]);*/
 
+        /*if($request->can_see == 1) {//この投稿の閲覧数を表示するときは閲覧数データ作る
+            $view = new View();
 
+            $view->create([
+                'username' => 'towa',
+                'edit_id' => $id,
+                'views' => 0,
+            ]);
+        } else {//この投稿の閲覧数を表示しないときは閲覧数データ消す
+            View::where('edit_id', $id)->delete();
+        }*/
+
+        return['ui' => $request->image];
         //あとでやる
 
         /*if($request->show_good == 1) {
