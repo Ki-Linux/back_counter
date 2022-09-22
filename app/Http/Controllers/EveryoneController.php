df7550994df05a142d17a9972609d020becf3aae
Ki-Linux <89332130+Ki-Linux@users.noreply.github.com>
2022-09-22 12:24:13 +0900

    comment add

--- app/Http/Controllers/EveryoneController.php
+++ app/Http/Controllers/EveryoneController.php
@@ -13,9 +13,8 @@ class EveryoneController extends Controller
     public function index(Request $request)
     {
         $username = $request->username;
-
         $account_comment = Account::where('username', $username)->first('comment');
-        return ['my_comment' => $account_comment];
         
+        return ['my_comment' => $account_comment];       
     }
 }
